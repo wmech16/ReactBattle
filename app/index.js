7a38dd26db13c3bdd2693b1b5b28bb642bf223e6
Billy Mech <billy@fanhero.com>
2017-05-17 17:50:01 -0400

    first commit

---
+++ app/index.js
@@ -0,0 +1,15 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var PropTypes = require('prop-types');
+require('./index.css');
+var App = require('./components/App');
+
+
+
+
+
+ReactDOM.render(
+	<App />,
+	document.getElementById('app')
+);
+
